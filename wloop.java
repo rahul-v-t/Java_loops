aec7c658ff2b9b85ff26580c60f6f76fc01a5812
rahulthambi10@gmail.com <rahulthambi10@gmail.com>
2021-11-22 17:26:56 +0530

    New one

---
+++ wloop.java
@@ -0,0 +1,11 @@
+public class wloop {
+	public static void main(String[] args) {
+		int value = 0;
+		
+		while(value<10)
+		{
+			System.out.println("Hello" + value);
+			value = value +1;
+		}
+	}
+}
